046c39e6afcdb3234a0660fb1e1ce0c220e54a5c
小鸡快跑 <2283572185@qq.com>
2024-06-30 17:39:33 +0800

    docs(readme/template): 修改文档并添加注释
    1. 在readme中增加对参数默认值的说明
    2. 在wrap.py中增加注释

--- template/wrap.py
+++ template/wrap.py
@@ -9,10 +9,21 @@ _email_ 2283572185@qq.com
 import sys
 import os
 
-arg_file = sys.argv[1]
-with open(arg_file, "r", encoding="utf8") as file:
-    word_dir = file.readline()
-    os.chdir(word_dir[:-1])
+"""*.wrap文件结构(utf8编码)
+work_dir
+command
+"""
+
+# 获取包装文件路径
+wrap_file = sys.argv[1]
+with open(wrap_file, encoding="utf8") as file:
+    # 加载工作目录
+    work_dir = file.readline()
+    os.chdir(work_dir[:-1])
+    # 运行包装文件中的命令
     command = file.readline()
+    # 回显编译命令
     print(command)
-    os.system(command)
+    exit_code = os.system(command)
+    # 将错误码返回给父进程
+    sys.exit(exit_code)
