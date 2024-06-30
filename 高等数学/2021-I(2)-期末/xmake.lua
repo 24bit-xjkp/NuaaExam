3eb54a5f9da0175feeb7bc88629b1ad4f9f34d3a
小鸡快跑 <2283572185@qq.com>
2024-06-30 13:36:19 +0800

    feat(高等数学): 支持多种答案输出模式
    1. 包装文件和试卷内容分离，实现多个相同内容的章节的生成
    2. 使用template中的章节风格设置来支持多种答案输出模式

---
+++ 高等数学/2021-I(2)-期末/xmake.lua
@@ -0,0 +1,9 @@
+-- @file xmake.lua
+-- @brief xmake构建脚本
+-- @detail 定义目标高等数学2021-I(2)-期末
+-- @author 24bit-xjkp
+-- @email 2283572185@qq.com
+
+target("高等数学2021-I(2)-期末")
+    add_files("*.tex")
+target_end()
