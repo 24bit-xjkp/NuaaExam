fd3b57535c3849ad0668095db9fcac4982ca73bd
小鸡快跑 <2283572185@qq.com>
2024-07-08 17:41:18 +0800

    feat(高等数学): 开始2022高等数学I(2)期末考题A排版
    1. 添加框架文件
    2. 添加xmake目标
    3. 同步原试卷

---
+++ AdvMath/2022-I(2)-Final-A/xmake.lua
@@ -0,0 +1,10 @@
+-- @file xmake.lua
+-- @brief xmake构建脚本
+-- @detail 定义目标高等数学2022-I(2)-期末-A
+-- @author 24bit-xjkp
+-- @email 2283572185@qq.com
+
+target("AdvMath2022-I(2)-Final-A")
+    add_files("*.tex")
+    set_basename("2022-I(2)-期末A")
+target_end()
